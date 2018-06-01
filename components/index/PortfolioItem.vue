04c53cc6c40f780c239a3d5e4f3b266103faa423
xolms111 <35960774+xolms111@users.noreply.github.com>
2018-06-01 12:41:12 +0300

    123

--- components/index/PortfolioItem.vue
+++ components/index/PortfolioItem.vue
@@ -1,16 +1,14 @@
 <template>
-  <transition :enter-active-class="index == 0 || index == 2 ? 'bounceInLeft' : 'bounceInRight'" :leave-active-class="index == 0 || index == 2 ? 'bounceOutLeft' : 'bounceOutRight'" name="bounce">
-    <nuxt-link :to="'/about/portfolio/' + item.alias">
-      <div class="item__image">
-        <img :src="item.image" :alt="item.name">
-        {{item.id}}
-      </div>
-      <div class="item__content">
-        <h3 class="item__name">{{item.name}}</h3>
-        <div class="item__types">{{item.type}}</div>
-      </div>
-    </nuxt-link>
-  </transition>
+  <nuxt-link :to="'/about/portfolio/' + item.alias">
+    <div class="item__image">
+      <img :src="item.image" :alt="item.name">
+      {{item.id}}
+    </div>
+    <div class="item__content">
+      <h3 class="item__name">{{item.name}}</h3>
+      <div class="item__types">{{item.type}}</div>
+    </div>
+  </nuxt-link>
 </template>
 
 <script>
